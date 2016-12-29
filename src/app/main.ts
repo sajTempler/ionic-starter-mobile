66f33cfe60f971b5b38d431651f7af77fe35ca0b
yozman <yozman@sina.com>
2016-12-30 05:24:58 +0800

    添加生产模式判断

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { enableProdMode } from '@angular/core'
 import { AppModule } from './app.module'
 
+(<any>window).cordova && enableProdMode()
 platformBrowserDynamic().bootstrapModule(AppModule)
