a4d8b85a7cc0e3d46458d7f70e5402c6aa111aea
yozman <yozman@sina.com>
2017-03-17 01:28:42 +0800

    优化代码

--- src/pages/sub/sub.ts
+++ src/pages/sub/sub.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core'
+import { User } from '../../imports/provider'
 
 @Component({
   selector: 'page-sub',
   templateUrl: 'sub.html'
 })
-export class SubPage {}
+export class SubPage {
+
+  constructor (
+    public user: User
+  ) {}
+
+  setLogout () {
+    this.user.onAir = false
+  }
+
+}
