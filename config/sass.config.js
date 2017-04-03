076d65054d4ab35bb9f2bd76f3269511a1114919
yozman <yozman@sina.com>
2017-04-03 15:27:16 +0800

    优化样式支持最低版本

--- config/sass.config.js
+++ config/sass.config.js
@@ -1,5 +1,13 @@
 config = require('@ionic/app-scripts/config/sass.config.js')
 
+config.autoprefixer = {
+  browsers: [
+    'last 2 versions',
+    'iOS >= 7',
+    'Android >= 4',
+  ]
+}
+
 config.excludeFiles = [
   /\.(md).(scss)$/i,
   /\.(wp).(scss)$/i
